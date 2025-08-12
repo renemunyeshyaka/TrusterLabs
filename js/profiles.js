cd385802feccdbfd9555d67b0cbb3968d2910a50
Jean René MUNYESHYAKA <munyeshyaka@hotmail.com>
2025-08-12 20:12:49 +0200

    # Profile updates

--- js/profiles.js
+++ js/profiles.js
@@ -5,7 +5,7 @@ let currentIndex = 1;
 
   function moveCarousel(direction) {
     const containerWidth = document.querySelector('.carousel-container').offsetWidth;
-    const profileWidth = profiles[0].offsetWidth + 35; // margin in px
+    const profileWidth = profiles[0].offsetWidth + 31; // margin in px
     const maxIndex = Math.max(0, totalProfiles - Math.floor(containerWidth / profileWidth));
     currentIndex += direction;
     if (currentIndex < 0) currentIndex = 0;
