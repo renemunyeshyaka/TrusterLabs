46e4623edecd418ec708cae47b68e68a0d07663e
Jean René MUNYESHYAKA <munyeshyaka@hotmail.com>
2025-08-12 22:43:34 +0200

    # Social Profile Modifications

--- js/profiles.js
+++ js/profiles.js
@@ -1,11 +1,11 @@
-let currentIndex = 1;
+let currentIndex = 0;
   const track = document.getElementById('team-carousel');
   const profiles = document.querySelectorAll('#team-carousel .profile');
   const totalProfiles = profiles.length;
 
   function moveCarousel(direction) {
     const containerWidth = document.querySelector('.carousel-container').offsetWidth;
-    const profileWidth = profiles[0].offsetWidth + 31; // margin in px
+    const profileWidth = profiles[0].offsetWidth + 30; // margin in px
     const maxIndex = Math.max(0, totalProfiles - Math.floor(containerWidth / profileWidth));
     currentIndex += direction;
     if (currentIndex < 0) currentIndex = 0;
